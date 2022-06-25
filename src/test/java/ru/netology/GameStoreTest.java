1186b55db6ade26eb37b2bde029510c7c4bfd0fe
Alexey Zimin <lexx.jock@gmail.com>
2022-06-25 21:28:53 +1000

    add test sumAddPlayTime

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -24,14 +24,25 @@ public class GameStoreTest {
     public void shouldPlayTime() {
 
         GameStore store = new GameStore();
-        Player player = new Player("Petya");
 
-        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
-        Game game1 = store.publishGame("Нетология Баттл Онлайн2", "Аркады2");
+        Game game1 = store.publishGame("Нетология Баттл Онлайн 1", "Аркады");
+        Game game2 = store.publishGame("Нетология Баттл Онлайн 2", "Аркады");
         store.addPlayTime("Petya", 1);
         store.addPlayTime("Vasya", 1);
         store.addPlayTime("Anna", 1);
         store.getMostPlayer();
+
+        GameStore store1 = new GameStore();
+
+        Game game3 = store1.publishGame("Уличный стрит", "Гонки");
+        Game game4 = store1.publishGame("Сматываемся от полицейских", "Гонки");
+        store1.addPlayTime("Petya", 3);
+        store1.addPlayTime("Anton", 1);
+        store1.addPlayTime("Lexx", 5);
+        store1.addPlayTime("Petya", 4);
+        store1.getMostPlayer();
+
+
 
     }
 
